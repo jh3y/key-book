420500f13d64979e53960be3ae5d42c70a751550
jh3y <jh3y@users.noreply.github.com>
2019-11-15 10:10:34 +0000

    feat: prefix + suffix

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const mirror = keys =>
-  Object.freeze(keys.reduce((obj, key) => ({ ...obj, [key]: key }), {}));
+const mirror = (keys, prefix = '', suffix = '') =>
+  Object.freeze(
+    keys.reduce(
+      (obj, key) => ({
+        ...obj,
+        [`${prefix}${key}${suffix}`]: `${prefix}${key}${suffix}`
+      }),
+      {}
+    )
+  );
 
 module.exports = mirror;
--- test.js
+++ test.js
@@ -12,4 +12,16 @@ describe("key-book", function() {
     enums.A = "C";
     assert.equal(enums.A, "A");
   });
+  it("adds prefix when supplied", function() {
+    const enums = mirror(["A", "B"]);
+    assert.equal(enums.A, "A");
+    const prefixedEnums = mirror(["A", "B"], "PREFIX__");
+    assert.equal(prefixedEnums.PREFIX__A, "PREFIX__A");
+  });
+  it("adds suffix when supplied", function() {
+    const enums = mirror(["A", "B"]);
+    assert.equal(enums.B, "B");
+    const suffixedEnums = mirror(["A", "B"], undefined, "__SUFFIX");
+    assert.equal(suffixedEnums.A__SUFFIX, "A__SUFFIX");
+  });
 });
